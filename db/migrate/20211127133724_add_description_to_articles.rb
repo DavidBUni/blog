ce1562db7c2f398e12eefffbc03de23cb7a68df4
DavidBUni <s5078285@bournemouth.ac.uk>
2021-11-27 13:59:00 +0000

    Fixed a issue I'd caused with migration

---
+++ db/migrate/20211127133724_add_description_to_articles.rb
@@ -0,0 +1,4 @@
+class AddDescriptionToArticles < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_225119) do
+ActiveRecord::Schema.define(version: 2021_11_27_133724) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
