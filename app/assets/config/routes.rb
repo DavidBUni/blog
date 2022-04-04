26e1375e334e1186d39fdb1e97a1a5077679564e
David O'Brien <s5078285@bournemouth.ac.uk>
2022-04-04 15:46:30 -0700

    Set up validation for articles and created an article controller and articles directory with a page to create articles which isn't working just yet

--- app/assets/config/routes.rb
+++ app/assets/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
-	root 'pages#index'
-
+ root 'pages#index'
+ get 'about', to: 'pages#about'
+ resources :articles
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
  root 'pages#index'
  get 'about', to: 'pages#about'
+ get 'articles', to: 'articles#new'
 end
