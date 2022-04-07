340c18c6f9344f4d60c61a5c241a470eae1c0f62
David O'Brien <s5078285@bournemouth.ac.uk>
2022-04-07 14:33:43 -0700

    Create a show.html.erb page to show the blog post just created and fixed a redirect issue I was having

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,15 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Article was successfully created"
-			redirect_to articles_show(@article)
+			redirect_to article_path(@article)
 		else
 			render 'new'
 		end
+	end
+	
+	
+	def show
+		@article = Article.find(params[:id])
 	end
 	
 	private
